9231c5e03cddffdb99a59331a9fdb0694843f4d9
Jesus Aguilar <jesus.aguilar@weightwatchers.com>
2020-03-11 23:36:04 -0300

    First commit for COVID-19 project.

---
+++ src/main/java/com/corecovid19/CoreCovid19Application.java
@@ -0,0 +1,13 @@
+package com.corecovid19;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreCovid19Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreCovid19Application.class, args);
+	}
+
+}
