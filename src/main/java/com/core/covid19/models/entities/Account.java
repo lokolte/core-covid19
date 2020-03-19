abe6c9c7739af1bc3f776d0a189a1f13c271e325
Jesus Aguilar <jesus.aguilar@weightwatchers.com>
2020-03-19 03:18:49 -0300

    Fix of serialization during insert for field password.

--- src/main/java/com/core/covid19/models/entities/Account.java
+++ src/main/java/com/core/covid19/models/entities/Account.java
@@ -5,6 +5,7 @@ import javax.persistence.*;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -29,7 +30,7 @@ public class Account implements Serializable {
 	private String email;
 
 	@Column(nullable=false, length=100)
-	@JsonIgnore
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	private String password;
 
 	//bi-directional many-to-one association to Person
