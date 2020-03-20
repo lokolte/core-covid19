5c7ba41a5d4932ccac02e347f3707bd30bc7bc27
Jesus Aguilar <jesus.aguilar@weightwatchers.com>
2020-03-20 00:09:13 -0300

    Added CORS support.

--- src/main/java/com/core/covid19/security/configs/SecurityConfigurer.java
+++ src/main/java/com/core/covid19/security/configs/SecurityConfigurer.java
@@ -11,6 +11,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.core.covid19.authentication.filters.JwtRequestFilter;
 import com.core.covid19.services.CustomUserDetailService;
@@ -32,12 +35,18 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.csrf().disable()
-				.authorizeRequests().antMatchers("/authentication/authenticate", "/accounts/singup").permitAll().
-						anyRequest().authenticated().and().sessionManagement()
-						.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-		
+		httpSecurity.cors().and().csrf().disable().authorizeRequests()
+				.antMatchers("/authentication/authenticate", "/accounts/singup").permitAll().anyRequest()
+				.authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+	}
+	
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+		return source;
 	}
 	
 	@Override
