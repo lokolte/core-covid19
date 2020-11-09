7bf6e61db74002245ed87b21dd5167ee5ad7499b
vgayoso <chuchosoft.239@gmail.com>
2020-11-09 19:36:09 -0300

    Dockerized service.

--- start.sh
+++ start.sh
@@ -8,8 +8,18 @@ echo "##########################################################################
 echo "############################################## DONE DOWNLOAD ##############################################"
 echo "###########################################################################################################"
 
-echo "Starting core-covid19 by jar..."
+# echo "Starting core-covid19 by jar..."
+# VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+# sudo java -jar target/core-covid19-${VERSION}.jar
 
-VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+echo "Deleting docker image if there exist..."
+docker rmi -f core-covid19_covid19.core
 
-sudo java -jar target/core-covid19-${VERSION}.jar
+echo "Cleaning system..."
+docker system prune -f
+
+echo "Starting to build docker image..."
+docker-compose build
+
+echo "Starting jar in docker image..."
+docker run --network="host" core-covid19_covid19.core
