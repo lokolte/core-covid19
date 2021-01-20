23ac1e4c4402d89fd6c9c5638adc858d295eb019
Federico Torres <federix.8190@gmail>
2021-01-19 22:02:19 -0300

    Se agrega servicio de respuestas para formularios

--- src/main/java/com/core/covid19/controllers/FormController.java
+++ src/main/java/com/core/covid19/controllers/FormController.java
@@ -1,13 +1,20 @@
 package com.core.covid19.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.core.covid19.authentication.util.JwtUtil;
+import com.core.covid19.models.entities.ItemsAnswer;
+import com.core.covid19.models.responses.AnswerItemResponse;
+import com.core.covid19.models.responses.PersonAnswersResponse;
 import com.core.covid19.models.responses.PersonFormsResponse;
 import com.core.covid19.services.FormService;
 
@@ -28,6 +35,17 @@ public class FormController {
 	@PostMapping
 	public void asign(@RequestHeader("Authorization") String authorization) {
 		formService.addDefaultFormsToPerson(jwtUtil.getEmailFromJwtToken(authorization));
+	}
+	
+	@GetMapping("/{idForm}/answers/{idPerson}")
+	public List<ItemsAnswer> getAnswersForm(
+			@PathVariable("idPerson") int idPerson,
+			@PathVariable("idForm") int idForm) {
+		
+		PersonAnswersResponse p = formService.getAnswersForm(idPerson, idForm);
+		if (p.getAnswers() != null && p.getAnswers().size() > 0)
+			return p.getAnswers().get(0).getAnswers();
+		return new ArrayList<ItemsAnswer>();
 	}
 
 }
--- src/main/java/com/core/covid19/services/FormService.java
+++ src/main/java/com/core/covid19/services/FormService.java
@@ -3,6 +3,7 @@ package com.core.covid19.services;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -10,13 +11,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.core.covid19.models.entities.Account;
+import com.core.covid19.models.entities.Answer;
 import com.core.covid19.models.entities.Form;
 import com.core.covid19.models.entities.Item;
+import com.core.covid19.models.entities.ItemsAnswer;
 import com.core.covid19.models.entities.Person;
+import com.core.covid19.models.responses.AnswerItemResponse;
 import com.core.covid19.models.responses.FormItemResponse;
+import com.core.covid19.models.responses.PersonAnswersResponse;
 import com.core.covid19.models.responses.PersonFormsResponse;
 import com.core.covid19.repos.AccountRepo;
 import com.core.covid19.repos.FormRepo;
+import com.core.covid19.repos.PersonRepo;
 
 @Service
 public class FormService {
@@ -26,6 +32,9 @@ public class FormService {
 	
 	@Autowired
 	private AccountRepo accountRepo;
+	
+	@Autowired
+	private PersonRepo personRepo;
 
 	public PersonFormsResponse findAllByPersonEmail(String email) {
 		Account account = accountRepo.findByEmail(email);
@@ -70,6 +79,29 @@ public class FormService {
 		forms.add(formRepo.findById(3).get()); // Formulario referente a COVID19
 
 		return forms;
+	}
+	
+	public PersonAnswersResponse getAnswersForm(int idPerson, int idForm) {
+
+		Optional<Person> p = personRepo.findById(idPerson);
+		if (p.isEmpty()) return null;
+		Person person = p.get();
+
+		List<AnswerItemResponse> answers = new ArrayList<AnswerItemResponse>();
+
+        for (Answer a : person.getPersonAnswers()) {
+        	int id = a.getForm().getId();
+	        if (id == idForm) {
+	        	List<ItemsAnswer> itemsAnswerList = new ArrayList<ItemsAnswer>();
+	        	for(ItemsAnswer ia : a.getAnswers())
+	        		itemsAnswerList.add(ia);
+	        	AnswerItemResponse answerItemResponse = new AnswerItemResponse(a.getId(), 
+	        			a.getForm(), a.getAnswerDate(), itemsAnswerList);
+	        	answers.add(answerItemResponse);
+        	}
+        }
+        answers = answers.stream().sorted().collect(Collectors.toList());
+		return new PersonAnswersResponse(answers);
 	}
 
 }
